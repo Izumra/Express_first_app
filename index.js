f952236212ae12f7dd38b5a366a7640150783729
Mark <zagumennikovmark@gmail.com>
2023-01-17 12:31:23 +0500

    footer

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 import express from "express"
 
-const port=3000
+const port=2000
 const app=express()
+app.set('view engine','ejs')
+app.use(express.static("public"))
 
-app.get('/',(req,res)=>res.send("Hello world!"))
+app.get('/',(req,res)=>res.render('homePage',{title:"Home page"}))
 app.listen(port)
