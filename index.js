eb89c672e6d99ff8e933f7eb9bf4f7f52bcabf9a
Mark <zagumennikovmark@gmail.com>
2023-01-18 12:06:22 +0500

    based routing and header

--- index.js
+++ index.js
@@ -5,5 +5,12 @@ const app=express()
 app.set('view engine','ejs')
 app.use(express.static("public"))
 
-app.get('/',(req,res)=>res.render('homePage',{title:"Home page"}))
+app.get('/',(req,res)=>res.render('homePage',{title:"Home page",user:null}))
+app.get('/Participants',(req,res)=>res.render('Participants',{title:"Participants page",user:null}))
+app.get('/Rating',(req,res)=>res.render('Rating',{title:"Rating page",user:null}))
+
 app.listen(port)
+
+
+//tests
+console.log("Server is starting")
